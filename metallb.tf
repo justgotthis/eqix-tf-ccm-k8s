dc9ef2d95c140bfac0ecaa596558c0f28724be66
Luis Lazo <theluislazois@gmail.com>
2020-10-27 21:17:35 -0500

    metallb worker count var change

--- metallb.tf
+++ metallb.tf
@@ -1,6 +1,6 @@
 # Enable BGP on each worker node
 resource "packet_bgp_session" "kube_bgp" {
-  count          = var.node_count
+  count          = var.worker_count
   device_id      = packet_device.k8s_workers.*.id[count.index]
   address_family = "ipv4"
 }
